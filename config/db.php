896c19fc7829becc30217491ab2e967839ba42db
Bedo <bedonassef71@gmail.com>
2024-11-03 02:00:36 +0200

    install packages

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => \Yiisoft\Db\Mysql\Connection::class,
+    'dsn' => 'mysql:host=localhost;dbname=school_app',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
